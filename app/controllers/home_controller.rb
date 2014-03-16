2f193bf0ad778d75fa28c0239478c9465c3d0d71
nzw3yl <roger.smith@ceryx.com>
2014-03-16 15:41:49 -0400

    added splash page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,13 @@
 class HomeController < ApplicationController
+  layout :home_layout
+
   def index
     @users = User.all
+    @user = current_user || User.new
   end
+
+  private
+    def home_layout
+      user_signed_in? ? "application" : "splash"
+    end
 end
