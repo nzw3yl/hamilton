060cfb1301e4460b79de2239263a894593b2d36a
nzw3yl <roger.smith@ceryx.com>
2014-03-19 12:27:14 -0400

    i18n on splash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,32 @@
 class ApplicationController < ActionController::Base
+
+   
   include Pundit
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  before_filter :set_locale  
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  
+
 
   private
 
   def user_not_authorized
     flash[:alert] = "Access denied."
     redirect_to (request.referrer || root_path)
+  end 
+  
+  def set_locale
+    I18n.locale = params[:locale] if params[:locale].present?
+    # current_user.locale
+    # request.subdomain
+    # request.env["HTTP_ACCEPT_LANGUAGE"]
+    # request.remote_ip
+  end
+
+  def default_url_options(options = {})
+    {locale: I18n.locale}
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Hamilton::Application.routes.draw do
-  root :to => "home#index"
-  devise_for :users, :controllers => {:registrations => "registrations"}
-  resources :users
+  scope ":locale", locale: /#{I18n.available_locales.join("|")}/ do
+    devise_for :users, :controllers => {:registrations => "registrations"}
+    resources :users
+    root to:  "home#index"
+  end
+  get '*path', to: redirect("/#{I18n.default_locale}/%{path}"), constraints: lambda { |req| !req.path.starts_with? "/#{I18n.default_locale}/" }
+  get '', to: redirect("/#{I18n.default_locale}")
 end
